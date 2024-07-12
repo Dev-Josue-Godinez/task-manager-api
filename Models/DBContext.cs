37207eee805f9fde5f2fdcbe27416aca0dd83468
Josue Godinez <josuegod1998.dev@gmail.com>
2024-07-12 00:10:41 -0600

    DbConnection & Controller LogIn

---
+++ Models/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Login> Logins { get; set; }
+    }
+}
---
+++ Task-Manager-API/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_Manager_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet, ActionName("Get")]
+        public string Get()
+        {
+            return "";
+        }
+
+        [HttpGet("Test"), ActionName("Test")]
+        public string Test()
+        {
+            return "";
+        }
+
+    }
+}
